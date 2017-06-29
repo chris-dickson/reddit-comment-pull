3595063af41af2eb00be9231ebcf8b4f35dd1f62
cdickson <cdickson@uncharted.software>
2017-06-29 12:17:54 -0400

    Serialize comments to json

--- reddit-comment-pull.py
+++ reddit-comment-pull.py
@@ -1,5 +1,6 @@
 import sys
 import praw
+import json
 
 client_id = sys.argv[1]
 client_secret = sys.argv[2]
@@ -10,5 +11,19 @@ reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,user_agent
 
 # Get the submission
 submission = reddit.submission(id='3g1jfi')
-for top_level_comment in submission.comments:
-    print(top_level_comment.body)
+submission.comments.replace_more(limit=0)
+for comment in submission.comments.list():
+    serializedComment = {}
+    serializedComment['type'] = 'Comment'
+    serializedComment['author'] = '[deleted]' if comment.author is None else comment.author.name
+    serializedComment['body'] = comment.body
+    serializedComment['created'] = comment.created_utc
+    serializedComment['score'] = comment.score
+    serializedComment['stickied'] = comment.stickied
+    serializedComment['parent_id'] = comment.parent_id
+    serializedComment['id'] = comment.id
+    serializedComment['edited'] = comment.edited
+    serializedComment['guilded'] = comment.gilded
+    serializedComment['permalink'] = submission.permalink + comment.id
+    commentJSON = json.dumps(serializedComment)
+    print(commentJSON)
