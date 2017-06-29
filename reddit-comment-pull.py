ececcd9a840e3f8a95b29fd275bde471159ef702
Chris Dickson <cdickson@uncharted.software>
2017-06-29 16:22:26 -0400

    Add functions for serializing comments

--- reddit-comment-pull.py
+++ reddit-comment-pull.py
@@ -6,13 +6,8 @@ client_id = sys.argv[1]
 client_secret = sys.argv[2]
 user_agent = 'reddit-comment-pull'
 
-# Create the reddit instance
-reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,user_agent=user_agent)
 
-# Get the submission
-submission = reddit.submission(id='3g1jfi')
-submission.comments.replace_more(limit=0)
-for comment in submission.comments.list():
+def serializeComment(comment):
     serializedComment = {}
     serializedComment['type'] = 'Comment'
     serializedComment['author'] = '[deleted]' if comment.author is None else comment.author.name
@@ -26,4 +21,25 @@ for comment in submission.comments.list():
     serializedComment['guilded'] = comment.gilded
     serializedComment['permalink'] = submission.permalink + comment.id
     commentJSON = json.dumps(serializedComment)
+    return commentJSON
+
+
+def serializeCommentsForSubmission(reddit, submissionid):
+    submission = reddit.submission(id=submissionid)
+    submission.comments.replace_more(limit=0)
+
+    comments = []
+    for comment in submission.comments.list():
+        commentJSON = serializeComment(comment=comment)
+        comments.append(commentJSON)
+
+
+# Create the reddit instance
+reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
+
+# Get the submission
+submission = reddit.submission(id='3g1jfi')
+submission.comments.replace_more(limit=0)
+for comment in submission.comments.list():
+    commentJSON = serializeComment(comment=comment)
     print(commentJSON)
